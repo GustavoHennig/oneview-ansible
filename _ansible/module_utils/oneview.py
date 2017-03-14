57fa0a9e7e6c5f6153a0473fadca414f56465927
Gustavo Augusto Hennig <gustavoh@dbserver.com.br>
2017-03-14 15:25:25 -0300

    Preparation to send the modules to the ansible core

---
+++ _ansible/module_utils/oneview.py
@@ -0,0 +1,296 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (2016) Hewlett Packard Enterprise Development LP
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible. If not, see <http://www.gnu.org/licenses/>.
+
+
+from __future__ import (absolute_import,
+                        division,
+                        print_function,
+                        unicode_literals)
+
+from future import standard_library
+from ansible.module_utils.basic import *
+from copy import deepcopy
+from collections import OrderedDict
+import json
+import logging
+
+standard_library.install_aliases()
+logger = logging.getLogger(__name__)
+
+try:
+    from hpOneView.oneview_client import OneViewClient
+    from hpOneView.exceptions import (HPOneViewException,
+                                      HPOneViewTaskError)
+
+    HAS_HPE_ONEVIEW = True
+except ImportError:
+    HAS_HPE_ONEVIEW = False
+
+
+class OneViewModuleBase(object):
+    HPE_ONEVIEW_SDK_REQUIRED = 'HPE OneView Python SDK is required for this module.'
+
+    ONEVIEW_COMMON_ARGS = dict(
+        config=dict(required=False, type='str')
+    )
+
+    ONEVIEW_VALIDATE_ETAG_ARGS = dict(
+        validate_etag=dict(
+            required=False,
+            type='bool',
+            default=True)
+    )
+
+    def __build_argument_spec(self, additional_arg_spec, validate_etag_support):
+
+        merged_arg_spec = dict()
+        merged_arg_spec.update(self.ONEVIEW_COMMON_ARGS)
+
+        if validate_etag_support:
+            merged_arg_spec.update(self.ONEVIEW_VALIDATE_ETAG_ARGS)
+
+        if additional_arg_spec:
+            merged_arg_spec.update(additional_arg_spec)
+
+        return merged_arg_spec
+
+    def __init__(self, additional_arg_spec=None, validate_etag_support=False):
+
+        argument_spec = self.__build_argument_spec(additional_arg_spec, validate_etag_support)
+
+        self.module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
+
+        self.__check_hpe_oneview()
+        self.__create_oneview_client()
+
+        self.state = self.module.params.get('state')
+        self.data = self.module.params.get('data')
+        self.validate_etag_support = validate_etag_support
+
+    def __check_hpe_oneview(self):
+        if not HAS_HPE_ONEVIEW:
+            self.module.fail_json(msg=self.HPE_ONEVIEW_SDK_REQUIRED)
+
+    def __create_oneview_client(self):
+        if not self.module.params['config']:
+            self.oneview_client = OneViewClient.from_environment_variables()
+        else:
+            self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+
+    def execute_module(self):
+        # Abstract function, must be implemented by inheritor
+        raise HPOneViewException("execute_module not implemented")
+
+    def run(self):
+        try:
+            if self.validate_etag_support:
+                if not self.module.params.get('validate_etag'):
+                    self.oneview_client.connection.disable_etag_validation()
+
+            result = self.execute_module()
+
+            if "changed" not in result:
+                result['changed'] = False
+
+            self.module.exit_json(**result)
+
+        except HPOneViewException as exception:
+            self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
+
+
+class ResourceComparator():
+    MSG_DIFF_AT_KEY = 'Difference found at key \'{0}\'. '
+
+    @staticmethod
+    def compare(first_resource, second_resource):
+        """
+        Recursively compares dictionary contents, ignoring type and order
+        Args:
+            first_resource: first dictionary
+            second_resource: second dictionary
+
+        Returns:
+            bool: True when equal, False when different.
+        """
+        resource1 = deepcopy(first_resource)
+        resource2 = deepcopy(second_resource)
+
+        debug_resources = "resource1 = {0}, resource2 = {1}".format(resource1, resource2)
+
+        # The first resource is True / Not Null and the second resource is False / Null
+        if resource1 and not resource2:
+            logger.debug("resource1 and not resource2. " + debug_resources)
+            return False
+
+        # Check all keys in first dict
+        for key in resource1.keys():
+            if key not in resource2:
+                # no key in second dict
+                if resource1[key] is not None:
+                    # key inexistent is equivalent to exist and value None
+                    logger.debug(ResourceComparator.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+            # If both values are null / empty / False
+            elif not resource1[key] and not resource2[key]:
+                continue
+            elif isinstance(resource1[key], dict):
+                # recursive call
+                if not ResourceComparator.compare(resource1[key], resource2[key]):
+                    # if different, stops here
+                    logger.debug(ResourceComparator.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+            elif isinstance(resource1[key], list):
+                # change comparison function (list compare)
+                if not ResourceComparator.compare_list(resource1[key], resource2[key]):
+                    # if different, stops here
+                    logger.debug(ResourceComparator.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+            elif ResourceComparator._standardize_value(resource1[key]) != ResourceComparator._standardize_value(
+                    resource2[key]):
+                # different value
+                logger.debug(ResourceComparator.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                return False
+
+        # Check all keys in second dict to find missing
+        for key in resource2.keys():
+            if key not in resource1:
+                # not exists in first dict
+                if resource2[key] is not None:
+                    # key inexistent is equivalent to exist and value None
+                    logger.debug(ResourceComparator.MSG_DIFF_AT_KEY.format(key) + debug_resources)
+                    return False
+
+        # no differences found
+        return True
+
+    @staticmethod
+    def compare_list(first_resource, second_resource):
+        """
+        Recursively compares lists contents, ignoring type
+        Args:
+            first_resource: first list
+            second_resource: second list
+
+        Returns:
+            True when equal;
+            False when different.
+
+        """
+
+        resource1 = deepcopy(first_resource)
+        resource2 = deepcopy(second_resource)
+
+        debug_resources = "resource1 = {0}, resource2 = {1}".format(resource1, resource2)
+
+        # The second list is null / empty  / False
+        if not resource2:
+            logger.debug("resource 2 is null. " + debug_resources)
+            return False
+
+        if len(resource1) != len(resource2):
+            # different length
+            logger.debug("resources have different length. " + debug_resources)
+            return False
+
+        resource1 = sorted(resource1, key=ResourceComparator._str_sorted)
+        resource2 = sorted(resource2, key=ResourceComparator._str_sorted)
+
+        for i, val in enumerate(resource1):
+            if isinstance(val, dict):
+                # change comparison function
+                if not ResourceComparator.compare(val, resource2[i]):
+                    logger.debug("resources are different. " + debug_resources)
+                    return False
+            elif isinstance(val, list):
+                # recursive call
+                if not ResourceComparator.compare_list(val, resource2[i]):
+                    logger.debug("lists are different. " + debug_resources)
+                    return False
+            elif ResourceComparator._standardize_value(val) != ResourceComparator._standardize_value(resource2[i]):
+                # value is different
+                logger.debug("values are different. " + debug_resources)
+                return False
+
+        # no differences found
+        return True
+
+    @staticmethod
+    def _str_sorted(obj):
+        if isinstance(obj, dict):
+            return json.dumps(obj, sort_keys=True)
+        else:
+            return str(obj)
+
+    @staticmethod
+    def _standardize_value(value):
+        """
+        Convert value to string to enhance the comparison.
+
+        Args:
+            value: Any object type.
+
+        Returns:
+            str: Converted value.
+        """
+        if isinstance(value, float) and value.is_integer():
+            # Workaround to avoid erroneous comparison between int and float
+            # Removes zero from integer floats
+            value = int(value)
+
+        return str(value)
+
+
+class ResourceMerger():
+    @staticmethod
+    def merge_list_by_key(original_list, updated_list, key, ignore_when_null=[]):
+        """
+        Merge two lists by the key. It basically:
+        1. Adds the items that are present on updated_list and are absent on original_list.
+        2. Removes items that are absent on updated_list and are present on original_list.
+        3. For all items that are in both lists, overwrites the values from the original item by the updated item.
+
+        Args:
+            original_list: original list.
+            updated_list: list with changes.
+            key: unique identifier.
+            ignore_when_null: list with the keys from the updated items that should be ignored in the merge, if its
+            values are null.
+        Returns:
+            list: Lists merged.
+        """
+        if not original_list:
+            return updated_list
+
+        items_map = OrderedDict([(i[key], i.copy()) for i in original_list])
+
+        merged_items = OrderedDict()
+
+        for item in updated_list:
+            item_key = item[key]
+            if item_key in items_map:
+                for ignored_key in ignore_when_null:
+                    if ignored_key in item and not item[ignored_key]:
+                        item.pop(ignored_key)
+                merged_items[item_key] = items_map[item_key].copy()
+                merged_items[item_key].update(item)
+            else:
+                merged_items[item_key] = item.copy()
+
+        return [val for (_, val) in merged_items.items()]
--- library/oneview_fc_network.py
+++ library/oneview_fc_network.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 ###
 # Copyright (2016) Hewlett Packard Enterprise Development LP
 #
@@ -15,16 +16,9 @@
 # limitations under the License.
 ###
 
-from ansible.module_utils.basic import *
-
-try:
-    from hpOneView.oneview_client import OneViewClient
-    from hpOneView.extras.comparators import resource_compare
-    from hpOneView.exceptions import HPOneViewException
-
-    HAS_HPE_ONEVIEW = True
-except ImportError:
-    HAS_HPE_ONEVIEW = False
+ANSIBLE_METADATA = {'status': ['stableinterface'],
+                    'supported_by': 'committer',
+                    'version': '1.0'}
 
 DOCUMENTATION = '''
 ---
@@ -32,38 +26,26 @@ module: oneview_fc_network
 short_description: Manage OneView Fibre Channel Network resources.
 description:
     - Provides an interface to manage Fibre Channel Network resources. Can create, update, and delete.
+version_added: "2.3"
 requirements:
     - "python >= 2.7.9"
     - "hpOneView >= 3.1.0"
 author: "Bruno Souza (@bsouza)"
 options:
-    config:
-      description:
-        - Path to a .json configuration file containing the OneView client configuration.
-          The configuration file is optional. If the file path is not provided, the configuration will be loaded from
-          environment variables.
-      required: false
     state:
         description:
             - Indicates the desired state for the Fibre Channel Network resource.
-              'present' will ensure data properties are compliant with OneView.
-              'absent' will remove the resource from OneView, if it exists.
+              C(present) will ensure data properties are compliant with OneView.
+              C(absent) will remove the resource from OneView, if it exists.
         choices: ['present', 'absent']
     data:
         description:
             - List with the Fibre Channel Network properties.
         required: true
-    validate_etag:
-        description:
-            - When the ETag Validation is enabled, the request will be conditionally processed only if the current ETag
-              for the resource matches the ETag provided in the data.
-        default: true
-        choices: ['true', 'false']
-notes:
-    - "A sample configuration file for the config parameter can be found at:
-       https://github.com/HewlettPackard/oneview-ansible/blob/master/examples/oneview_config-rename.json"
-    - "Check how to use environment variables for configuration at:
-       https://github.com/HewlettPackard/oneview-ansible#environment-variables"
+
+extends_documentation_fragment:
+    - oneview
+    - oneview.validateetag
 '''
 
 EXAMPLES = '''
@@ -97,98 +79,72 @@ fc_network:
     type: complex
 '''
 
+from ansible.module_utils.basic import *
+from _ansible.module_utils.oneview import OneViewModuleBase, ResourceComparator
+
 FC_NETWORK_CREATED = 'FC Network created successfully.'
 FC_NETWORK_UPDATED = 'FC Network updated successfully.'
 FC_NETWORK_DELETED = 'FC Network deleted successfully.'
 FC_NETWORK_ALREADY_EXIST = 'FC Network already exists.'
 FC_NETWORK_ALREADY_ABSENT = 'Nothing to do.'
-HPE_ONEVIEW_SDK_REQUIRED = 'HPE OneView Python SDK is required for this module.'
 
 
-class FcNetworkModule(object):
-    argument_spec = dict(
-        config=dict(required=False, type='str'),
-        state=dict(
-            required=True,
-            choices=['present', 'absent']
-        ),
-        data=dict(required=True, type='dict'),
-        validate_etag=dict(
-            required=False,
-            type='bool',
-            default=True)
-    )
-
+class FcNetworkModule(OneViewModuleBase):
     def __init__(self):
-        self.module = AnsibleModule(argument_spec=self.argument_spec, supports_check_mode=False)
-        if not HAS_HPE_ONEVIEW:
-            self.module.fail_json(msg=HPE_ONEVIEW_SDK_REQUIRED)
 
-        if not self.module.params['config']:
-            self.oneview_client = OneViewClient.from_environment_variables()
-        else:
-            self.oneview_client = OneViewClient.from_json_file(self.module.params['config'])
+        add_arg_spec = dict(data=dict(required=True, type='dict'),
+                            state=dict(
+                                required=True,
+                                choices=['present', 'absent']))
 
-    def run(self):
-        state = self.module.params['state']
-        data = self.module.params['data']
+        super(FcNetworkModule, self).__init__(additional_arg_spec=add_arg_spec,
+                                              validate_etag_support=True)
 
-        try:
-            if not self.module.params.get('validate_etag'):
-                self.oneview_client.connection.disable_etag_validation()
+    def execute_module(self):
+        resource = self.__get_by_name()
 
-            if state == 'present':
-                self.__present(data)
-            elif state == 'absent':
-                self.__absent(data)
+        if self.state == 'present':
+            return self.__present(resource)
+        elif self.state == 'absent':
+            return self.__absent(resource)
 
-        except HPOneViewException as exception:
-            self.module.fail_json(msg='; '.join(str(e) for e in exception.args))
-
-    def __present(self, data):
-        resource = self.__get_by_name(data)
+    def __present(self, resource):
+        changed = False
+        if "newName" in self.data:
+            self.data["name"] = self.data.pop("newName")
 
         if not resource:
-            self.__create(data)
-        else:
-            self.__update(data, resource)
+            resource = self.oneview_client.fc_networks.create(self.data)
+            msg = FC_NETWORK_CREATED
+            changed = True
 
-    def __absent(self, data):
-        resource = self.__get_by_name(data)
+        else:
+            merged_data = resource.copy()
+            merged_data.update(self.data)
+
+            if ResourceComparator.compare(resource, merged_data):
+                msg = FC_NETWORK_ALREADY_EXIST
+            else:
+                resource = self.oneview_client.fc_networks.update(merged_data)
+                changed = True
+                msg = FC_NETWORK_UPDATED
+
+        return dict(
+            msg=msg,
+            changed=changed,
+            ansible_facts=dict(fc_network=resource)
+        )
+
+    def __absent(self, resource):
 
         if resource:
             self.oneview_client.fc_networks.delete(resource)
-            self.module.exit_json(changed=True,
-                                  msg=FC_NETWORK_DELETED)
+            return {"changed": True, "msg": FC_NETWORK_DELETED}
         else:
-            self.module.exit_json(changed=False, msg=FC_NETWORK_ALREADY_ABSENT)
+            return {"changed": False, "msg": FC_NETWORK_ALREADY_ABSENT}
 
-    def __create(self, data):
-        new_fc_network = self.oneview_client.fc_networks.create(data)
-
-        self.module.exit_json(changed=True,
-                              msg=FC_NETWORK_CREATED,
-                              ansible_facts=dict(fc_network=new_fc_network))
-
-    def __update(self, data, resource):
-        merged_data = resource.copy()
-        merged_data.update(data)
-
-        if resource_compare(resource, merged_data):
-
-            self.module.exit_json(changed=False,
-                                  msg=FC_NETWORK_ALREADY_EXIST,
-                                  ansible_facts=dict(fc_network=resource))
-
-        else:
-            updated_fc_network = self.oneview_client.fc_networks.update(merged_data)
-
-            self.module.exit_json(changed=True,
-                                  msg=FC_NETWORK_UPDATED,
-                                  ansible_facts=dict(fc_network=updated_fc_network))
-
-    def __get_by_name(self, data):
-        result = self.oneview_client.fc_networks.get_by('name', data['name'])
+    def __get_by_name(self):
+        result = self.oneview_client.fc_networks.get_by('name', self.data['name'])
         return result[0] if result else None
 
 
